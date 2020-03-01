56e1d2205d35a7cc900fb60f81c46d6d39bf86e3
Jarjanazy <celil.cercenazi@gmail.com>
2020-03-01 15:19:59 +0300

    added security and spring data jpa maven dependecies, created an empty securityConfig class

---
+++ Backend/survey/src/main/java/com/codeit/survey/configurations/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.codeit.survey.configurations;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
