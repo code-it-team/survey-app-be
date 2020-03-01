092658adfd905b94784414d696c1c1de7adc67b6
Jarjanazy <celil.cercenazi@gmail.com>
2020-03-01 22:24:21 +0300

    added the SurveyUser and Authority entities

---
+++ Backend/survey/src/main/java/com/codeit/survey/entities/Authority.java
@@ -0,0 +1,27 @@
+package com.codeit.survey.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter @Setter @NoArgsConstructor
+@Entity(name = "auth")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String role;
+
+    @OneToMany(mappedBy = "authority")
+    List<SurveyUser> users;
+
+    public Authority(String role, List<SurveyUser> users) {
+        this.role = role;
+        this.users = users;
+    }
+}
---
+++ Backend/survey/src/main/java/com/codeit/survey/entities/SurveyUser.java
@@ -0,0 +1,31 @@
+package com.codeit.survey.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter @Setter @NoArgsConstructor
+@Entity(name = "survey_user")
+public class SurveyUser{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+
+    String username;
+    String password;
+    boolean enabled;
+    @ManyToOne()
+    Authority authority;
+
+    public SurveyUser(String username, String password, boolean enabled, Authority authority) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.authority = authority;
+    }
+}
