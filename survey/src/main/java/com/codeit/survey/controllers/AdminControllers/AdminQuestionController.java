322a392b7ba4810264ff122d6b4a13679d48fca3
Jarjanazy <celil.cercenazi@gmail.com>
2020-04-02 01:07:58 +0300

    Delete the AdminQuestionService and move its functionality to QuestionService

--- survey/src/main/java/com/codeit/survey/controllers/AdminControllers/AdminQuestionController.java
+++ survey/src/main/java/com/codeit/survey/controllers/AdminControllers/AdminQuestionController.java
@@ -1,7 +1,6 @@
 package com.codeit.survey.controllers.AdminControllers;
 
 import com.codeit.survey.entities.Question;
-import com.codeit.survey.services.AdminServices.AdminQuestionService;
 import com.codeit.survey.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,26 +11,26 @@ import java.util.List;
 @RestController
 public class AdminQuestionController {
 
-    private AdminQuestionService questionService;
+    private QuestionService questionService;
 
     @Autowired
-    public  AdminQuestionController(AdminQuestionService questionService){
+    public  AdminQuestionController(QuestionService questionService){
         this.questionService = questionService;
     }
 
     @PostMapping("admin/SurveyAddQuestions")
     public ResponseEntity<?> addQuestionsToSurvey(@RequestBody List<Question> questions){
-        return questionService.addQuestionsToSurvey(questions);
+        return questionService.addQuestionsToSurvey_admin(questions);
     }
 
     @DeleteMapping("admin/SurveyDeleteQuestions")
     public ResponseEntity<?> deleteQuestionsFromSurvey(@RequestBody List<Question> questions){
-        return questionService.deleteQuestionsFromSurvey(questions);
+        return questionService.deleteQuestionsFromSurvey_admin(questions);
     }
 
     @PutMapping("admin/SurveyUpdateQuestions")
     public ResponseEntity<?> updateSurveyQuestions(@RequestBody List<Question> questions){
-        return questionService.updateSurveyQuestion(questions);
+        return questionService.updateSurveyQuestion_admin(questions);
     }
 
 
--- survey/src/main/java/com/codeit/survey/services/QuestionService.java
+++ survey/src/main/java/com/codeit/survey/services/QuestionService.java
@@ -10,6 +10,7 @@ import com.codeit.survey.security.CustomUserDetails;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -62,6 +63,20 @@ public class QuestionService {
     }
 
     /**
+     * questions in newQuestions list must have their ID set
+     * */
+    private void updateQuestions(List<Question> newQuestions){
+        for (Question newQuestion : newQuestions){
+            Question question = questionRepo.findById(newQuestion.getId()).orElseThrow(RuntimeException::new);
+            if(newQuestion.getBody() != null){
+                question.setBody(newQuestion.getBody());
+            }
+            questionRepo.save(question);
+        }
+    }
+
+
+    /**
      * @return true if all the questions belong to surveys of the user
      */
     private boolean isUserSurvey(List<Question> questions, Authentication auth){
@@ -85,13 +100,9 @@ public class QuestionService {
         return questionsNotOfUser.size() == 0;
     }
 
-    public ResponseEntity<?> addQuestionsToSurvey(List<Question> questions){
-        // do the surveys of the questions belong to the user
-        if (! isUserSurvey(questions, SecurityContextHolder.getContext().getAuthentication())){
-            return ResponseEntity.badRequest().build();
-        }
-
-       try{
+    // admin methods
+    public ResponseEntity<?> addQuestionsToSurvey_admin(List<Question> questions){
+        try{
             setSurvey(questions);
             setChoices(questions);
 
@@ -102,7 +113,7 @@ public class QuestionService {
         }
     }
 
-    public ResponseEntity<?> deleteQuestionsFromSurvey(List<Question> questions){
+    public ResponseEntity<?> deleteQuestionsFromSurvey_admin(List<Question> questions){
         try {
             questionRepo.deleteAll(questions);
             return ResponseEntity.ok().build();
@@ -111,26 +122,40 @@ public class QuestionService {
         }
     }
 
-    /**
-     * questions in newQuestions list must have their ID set
-     * */
-    private void updateQuestions(List<Question> newQuestions){
-        for (Question newQuestion : newQuestions){
-            Question question = questionRepo.findById(newQuestion.getId()).orElseThrow(RuntimeException::new);
-            if(newQuestion.getBody() != null){
-                question.setBody(newQuestion.getBody());
-            }
-            questionRepo.save(question);
-        }
-    }
-
-    public ResponseEntity<?> updateSurveyQuestion(List<Question> questions){
+    public ResponseEntity<?> updateSurveyQuestion_admin(List<Question> questions){
         try {
             updateQuestions(questions);
             return ResponseEntity.ok().build();
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
         }
+    }
+
+
+    // user methods
+    public ResponseEntity<?> addQuestionsToSurvey(List<Question> questions){
+        // do the surveys of the questions belong to the user
+        if (! isUserSurvey(questions, SecurityContextHolder.getContext().getAuthentication())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        // if the user has proven that he owns all the questions, he can add as an admin
+        return addQuestionsToSurvey_admin(questions);
+    }
+
+    public ResponseEntity<?> deleteQuestionsFromSurvey(List<Question> questions){
+        // do the surveys of the questions belong to the user
+        if (! isUserSurvey(questions, SecurityContextHolder.getContext().getAuthentication())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return deleteQuestionsFromSurvey_admin(questions);
+    }
+
+    public ResponseEntity<?> updateSurveyQuestion(List<Question> questions){
+        // do the surveys of the questions belong to the user
+        if (! isUserSurvey(questions, SecurityContextHolder.getContext().getAuthentication())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return updateSurveyQuestion_admin(questions);
     }
 
 }
