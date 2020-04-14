3e29d387f1ffeeeaf4d3c7d1a48797b59d15b17a
Jarjanazy <celil.cercenazi@gmail.com>
2020-04-14 22:26:40 +0300

    Make the adminSeeder work only in dev profile

--- survey/src/main/java/com/codeit/survey/startupRunners/AdminSeeder.java
+++ survey/src/main/java/com/codeit/survey/startupRunners/AdminSeeder.java
@@ -8,6 +8,7 @@ import com.codeit.survey.repositories.UserRepo;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("prod")
 public class AdminSeeder implements CommandLineRunner {
     UserRepo userRepo;
     AuthorityRepo authorityRepo;
