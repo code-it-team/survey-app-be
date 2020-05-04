53995f2f30cfcb4728e31846962baf81e8d5712b
Jarjanazy <celil.cercenazi@gmail.com>
2020-05-04 03:38:13 +0300

    Fix for the allowing of all request methods through the CORS policy

--- survey/src/main/java/com/codeit/survey/configurations/WebConfig.java
+++ survey/src/main/java/com/codeit/survey/configurations/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
     }
 }
